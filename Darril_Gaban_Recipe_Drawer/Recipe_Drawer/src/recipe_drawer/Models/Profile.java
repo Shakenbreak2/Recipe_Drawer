955a823d34cd052999b198a6ddf1df5338b6a93f
Shakenbreak2 <57147614+Shakenbreak2@users.noreply.github.com>
2020-01-10 13:31:32 -0600

    added some methods

--- Darril_Gaban_Recipe_Drawer/Recipe_Drawer/src/recipe_drawer/Models/Profile.java
+++ Darril_Gaban_Recipe_Drawer/Recipe_Drawer/src/recipe_drawer/Models/Profile.java
@@ -1,9 +1,76 @@
 package recipe_drawer.Models;
 
 public class Profile {
-
+	private String username;
+	private String email;
+	private String name;
+	private String bio;
+	
+	public Profile(String username, String email, String name, String bio) {
+		this.setBio(bio);
+		this.setEmail(email);
+		this.setName(name);
+		this.setUsername(username);
+	}
+	
 	public Profile() {
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the bio
+	 */
+	public String getBio() {
+		return bio;
+	}
+
+	/**
+	 * @param bio the bio to set
+	 */
+	public void setBio(String bio) {
+		this.bio = bio;
 	}
 
 }
